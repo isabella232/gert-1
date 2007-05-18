4a29d7c33cd02cf2909b4802a35b0226a5ac3920
Gert Driesen <drieseng@users.sourceforge.net>
2007-05-18 18:35:14 +0000

    Added test for bug #81677.
    svn path=/trunk/gert/; revision=77662

---
+++ interactive/bug81677/MainForm.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+public class MainForm : Form
+{
+	public MainForm ()
+	{
+		// 
+		// _richTextBox
+		// 
+		_richTextBox = new RichTextBox ();
+		_richTextBox.Dock = DockStyle.Fill;
+		Controls.Add (_richTextBox);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (300, 200);
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #81677";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		_richTextBox.Text = "Some Text That Should Allow Us To Test The " +
+			"Visual Effect Of The WordWrap property.";
+
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private RichTextBox _richTextBox;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Expected result on start-up:{0}{0}" +
+			"1. The following text is fully visible (but spread over multiple " +
+			"lines) in the RichTextBox:{0}{0}" +
+			"   Some Text That Should Allow Us To Test The Visual Effect Of{0}" +
+			"   The WordWrap property",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// _bugDescriptionText2
+		// 
+		_bugDescriptionText2 = new TextBox ();
+		_bugDescriptionText2.Multiline = true;
+		_bugDescriptionText2.Dock = DockStyle.Fill;
+		_bugDescriptionText2.Text = string.Format (CultureInfo.InvariantCulture,
+			"Steps to execute:{0}{0}" +
+			"1. Resize the form.{0}{0}" +
+			"Expected result:{0}{0}" +
+			"1. The following text remains fully visible at all times (but possibly " +
+			"spread over multiple lines):{0}{0}" +
+			"   Some Text That Should Allow Us To Test The Visual Effect Of{0}" +
+			"   The WordWrap property",
+			Environment.NewLine);
+		// 
+		// _tabPage2
+		// 
+		_tabPage2 = new TabPage ();
+		_tabPage2.Text = "#2";
+		_tabPage2.Controls.Add (_bugDescriptionText2);
+		_tabControl.Controls.Add (_tabPage2);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (360, 210);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #81677";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TextBox _bugDescriptionText2;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+	private TabPage _tabPage2;
+}
