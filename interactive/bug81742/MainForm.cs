832a4797445ca2165ba52792fcba43d7e864e7c2
Gert Driesen <drieseng@users.sourceforge.net>
2007-05-27 09:17:11 +0000

    Added test for bug #81742.
    svn path=/trunk/gert/; revision=78001

---
+++ interactive/bug81742/MainForm.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+class MainForm : Form
+{
+	public MainForm ()
+	{
+		// 
+		// _monthCalendar
+		// 
+		_monthCalendar = new MonthCalendar ();
+		_monthCalendar.Location = new Point (10, 10);
+		Controls.Add (_monthCalendar);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (180, 180);
+		Location = new Point (300, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #81742";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private MonthCalendar _monthCalendar;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Expected result on start-up:{0}{0}" +
+			"1. A MonthCalendar is displayed at {{X=10,Y=10}}.",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// _bugDescriptionText2
+		// 
+		_bugDescriptionText2 = new TextBox ();
+		_bugDescriptionText2.Dock = DockStyle.Fill;
+		_bugDescriptionText2.Multiline = true;
+		_bugDescriptionText2.Text = string.Format (CultureInfo.InvariantCulture,
+			"Steps to execute:{0}{0}" +
+			"1. Click on the form.{0}{0}" +
+			"2. Press Alt-F4.{0}{0}" +
+			"Expected result:{0}{0}" +
+			"1. The form is closed.",
+			Environment.NewLine);
+		// 
+		// _tabPage2
+		// 
+		_tabPage2 = new TabPage ();
+		_tabPage2.Text = "#2";
+		_tabPage2.Controls.Add (_bugDescriptionText2);
+		_tabControl.Controls.Add (_tabPage2);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (360, 160);
+		Location = new Point (550, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #81742";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TextBox _bugDescriptionText2;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+	private TabPage _tabPage2;
+}
