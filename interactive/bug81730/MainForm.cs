bb972d6f3e44cfdffd5bf3e32990711f2c5ad7cf
Gert Driesen <drieseng@users.sourceforge.net>
2007-05-28 11:28:02 +0000

    Added test for bug #81730.
    svn path=/trunk/gert/; revision=78048

---
+++ interactive/bug81730/MainForm.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+public class MainForm : Form
+{
+	public MainForm ()
+	{
+		// 
+		// _eventsText
+		// 
+		_eventsText = new TextBox ();
+		_eventsText.Dock = DockStyle.Bottom;
+		_eventsText.Height = 150;
+		_eventsText.Multiline = true;
+		Controls.Add (_eventsText);
+		// 
+		// MainForm
+		// 
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #81730";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	protected override void OnMouseDown (MouseEventArgs e)
+	{
+		base.OnMouseDown (e);
+		_eventsText.AppendText ("Drap&Drop => Start" + Environment.NewLine);
+		DoDragDrop ("me!", DragDropEffects.Move);
+		_eventsText.AppendText ("Drap&Drop => End" + Environment.NewLine);
+	}
+
+	private TextBox _eventsText;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Steps to execute:{0}{0}" +
+			"1. Click and hold the mouse button on the top part of the form.{0}{0}" +
+			"2. Release the mouse button.{0}{0}" +
+			"Expected result:{0}{0}" +
+			"1. On step 1, the following line is written to the TextBox:{0}{0}" +
+			"   Drag&Drop => Start{0}{0}" +
+			"2. On step 2, the following line is written to the TextBox:{0}{0}" +
+			"   Drag&Drop => End",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (360, 160);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #81730";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+}
