31d1e9f77e54163ca2577574fe44ca0378030e4f
Gert Driesen <drieseng@users.sourceforge.net>
2007-06-09 08:36:29 +0000

    Added test for bug #81818.
    svn path=/trunk/gert/; revision=79039

---
+++ standalone/bug81818/test.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class MyClass
+{
+	public class A
+	{
+		public event EventHandler MouseClick;
+
+		public virtual void Fire ()
+		{
+			if (MouseClick != null)
+				MouseClick (this, new EventArgs ());
+		}
+	}
+
+	public class B : A
+	{
+		public new event EventHandler MouseClick;
+
+		public override void Fire ()
+		{
+			if (MouseClick != null)
+				MouseClick (this, new EventArgs ());
+		}
+	}
+
+	public class C : B
+	{
+		public new void MouseClick ()
+		{
+			Console.Write ("This should be printed");
+		}
+	}
+
+	static public void Main ()
+	{
+		C myclass = new C ();
+		myclass.MouseClick ();
+	}
+}
