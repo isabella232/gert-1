fa3249eee2d86760ebcc95f2a9cf83bb180f5e5c
Gert Driesen <drieseng@users.sourceforge.net>
2007-06-11 17:18:04 +0000

    Added test for bug #81855.
    svn path=/trunk/gert/; revision=79196

---
+++ standalone/bug81855/test.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using StringList = System.Collections.Generic.List<string>;
+
+[assembly: AssemblyTitle ("Monodocer - The Mono Documentation Tool")]
+
+class Test
+{
+	static void Main ()
+	{
+	}
+}
