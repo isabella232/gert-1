9c7aa8f93597f3197f4088fc3249dfd3bab5d8de
Gert Driesen <drieseng@users.sourceforge.net>
2007-07-08 11:47:51 +0000

    Added test for bug #82040.
    svn path=/trunk/gert/; revision=81589

---
+++ standalone/bug82040/test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+	[STAThread]
+	static void Main ()
+	{
+		Form form = new Form ();
+		form.ShowInTaskbar = false;
+
+		TableLayoutPanel tableLayoutPanel = new TableLayoutPanel ();
+		tableLayoutPanel.ColumnCount = 3;
+		tableLayoutPanel.Dock = DockStyle.Fill;
+		tableLayoutPanel.RowCount = 11;
+		form.Controls.Add (tableLayoutPanel);
+
+		Timer timer = new Timer ();
+		timer.Interval = 100;
+		timer.Tick += delegate (object sender, EventArgs e) {
+			form.Close ();
+		};
+
+		form.Load += delegate (object sender, EventArgs e) {
+			timer.Enabled = true;
+		};
+		form.ShowDialog ();
+	}
+}
