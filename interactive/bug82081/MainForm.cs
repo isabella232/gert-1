d7bcefb4a45d3a9c529e39e6aaef51c7aa47e44d
Gert Driesen <drieseng@users.sourceforge.net>
2007-07-13 03:38:59 +0000

    Added test for bug #82081.
    svn path=/trunk/gert/; revision=81920

---
+++ interactive/bug82081/MainForm.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+public class MainForm : Form
+{
+	public MainForm ()
+	{
+		_flatButton = new Button ();
+		_flatButton.FlatAppearance.BorderSize = 0;
+		_flatButton.FlatStyle = FlatStyle.Flat;
+		_flatButton.Location = new Point (120, 20);
+		_flatButton.Text = "OK";
+		Controls.Add (_flatButton);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (300, 70);
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #82081";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private Button _flatButton;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Expected result on start-up:{0}{0}" +
+			"1. The button is flat.{0}{0}" +
+			"2. The button has no border.{0}{0}" +
+			"3. The button highlights on mouseover.",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (360, 150);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #82081";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+}
