65459d6b28cd414273d56805292a3ccf7e5ebd1b
Gert Driesen <drieseng@users.sourceforge.net>
2007-07-16 13:10:46 +0000

    Added test for bug #81932.
    svn path=/trunk/gert/; revision=82029

---
+++ standalone/bug81932/test.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+class Program
+{
+	static int Main ()
+	{
+		string xml = "<root><child1><nest1><nest2>hello!</nest2></nest1></child1><child2/><child3/></root>";
+		XmlReader r = new XmlTextReader (new StringReader (xml));
+
+		while (r.Read ()) {
+			if (r.Name == "child1")
+				break;
+		}
+		XPathDocument d = new XPathDocument (r);
+		XPathNavigator n = d.CreateNavigator ();
+#if MONO
+		string expected = string.Format ("<nest1>{0}  <nest2>hello!</nest2>{0}</nest1>",
+			Environment.NewLine);
+#else
+		// https://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=287614
+		string expected = string.Format ("<child1>{0}  <nest1>{0}    <nest2>hello!</nest2>{0}  </nest1>{0}</child1>{0}<child2 />{0}<child3 />",
+			Environment.NewLine);
+#endif
+
+		if (n.OuterXml != expected) {
+			Console.WriteLine ("Expected result:");
+			Console.WriteLine (expected);
+			Console.WriteLine ();
+			Console.WriteLine ("Actual result:");
+			Console.WriteLine (n.OuterXml);
+			return 1;
+		}
+		return 0;
+	}
+}
