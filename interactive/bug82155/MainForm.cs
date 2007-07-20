e6308a3aaced5ea617a00c718bccc57b73ea1afe
Gert Driesen <drieseng@users.sourceforge.net>
2007-07-20 19:49:09 +0000

    Added test for bug #82155.
    svn path=/trunk/gert/; revision=82385

---
+++ interactive/bug82155/MainForm.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+public class MainForm : Form
+{
+	public MainForm ()
+	{
+		_treeView = new TreeView ();
+		_treeView.Dock = DockStyle.Top;
+		_treeView.Height = 70;
+		Controls.Add (_treeView);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (292, 80);
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #82155";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		TreeNode treeNode1 = new TreeNode ("Node1");
+		TreeNode treeNode0 = new TreeNode ("Node0", new TreeNode [] { treeNode1 });
+		_treeView.Nodes.Add (treeNode0);
+		_treeView.ExpandAll ();
+
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private TreeView _treeView;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Location = new Point (8, 8);
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Expected result on start-up:{0}{0}" +
+			"1. Node0 is expanded.{0}{0}" +
+			"2. No vertical scrollbar in the TreeView.",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (330, 110);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #82155";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+}
