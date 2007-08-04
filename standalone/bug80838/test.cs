23571ce5110641f426010e937cf0f8b9615e0bcb
Gert Driesen <drieseng@users.sourceforge.net>
2007-08-04 12:29:09 +0000

    Fix test.
    svn path=/trunk/gert/; revision=83472

--- standalone/bug80838/test.cs
+++ standalone/bug80838/test.cs
@@ -32,9 +32,10 @@ class Program
 		pinfo.RedirectStandardOutput = true;
 
 		Process p = Process.Start (pinfo);
-		if (p.StandardOutput.CurrentEncoding != Console.OutputEncoding)
+		if (p.StandardOutput.CurrentEncoding.BodyName != Console.OutputEncoding.BodyName) {
 			return 2;
-		if (p.StandardInput.Encoding != Console.InputEncoding)
+		}
+		if (p.StandardInput.Encoding.BodyName != Console.InputEncoding.BodyName)
 			return 3;
 		return 0;
 	}
