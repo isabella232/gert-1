f8f8706d2738f0a2c20fe059de57fe06370e174d
Gert Driesen <drieseng@users.sourceforge.net>
2007-08-09 17:08:49 +0000

    Added test for bug #82226.
    svn path=/trunk/gert/; revision=83774

---
+++ interactive/bug82226/MainForm.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+class MainForm : Form
+{
+	public MainForm ()
+	{
+		// 
+		// _dataGridView
+		// 
+		_dataGridView = new DataGridView ();
+		_dataGridView.Dock = DockStyle.Fill;
+		Controls.Add (_dataGridView);
+		// 
+		// _nameTextBoxColumn
+		// 
+		_nameTextBoxColumn = new DataGridViewTextBoxColumn ();
+		_nameTextBoxColumn.HeaderText = "Name";
+		_dataGridView.Columns.Add (_nameTextBoxColumn);
+		// 
+		// _firstNameTextBoxColumn
+		// 
+		_firstNameTextBoxColumn = new DataGridViewTextBoxColumn ();
+		_firstNameTextBoxColumn.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+		_firstNameTextBoxColumn.HeaderText = "First Name";
+		_dataGridView.Columns.Add (_firstNameTextBoxColumn);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (300, 200);
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #82226";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		_dataGridView.Rows.Add ("de Icaza", "Miguel");
+		_dataGridView.Rows.Add ("Toshok", "Chris");
+		_dataGridView.Rows.Add ("Harper", "Jackson");
+
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private DataGridView _dataGridView;
+	private DataGridViewTextBoxColumn _nameTextBoxColumn;
+	private DataGridViewTextBoxColumn _firstNameTextBoxColumn;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Expected result on start-up:{0}{0}" +
+			"1. The DataGridView contains 4 rows.{0}{0}" +
+			"2. The last row is empty, and can be used to insert a new row.",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (320, 110);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #82226";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+}
