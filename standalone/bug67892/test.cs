f894ee481dfb4d0a9b54aa43a4c8ef9017974e00
Gert Driesen <drieseng@users.sourceforge.net>
2007-08-14 14:01:22 +0000

    Added test for bug #67909 and #67892.
    svn path=/trunk/gert/; revision=84077

---
+++ standalone/bug67892/test.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+
+class TinyHost : MarshalByRefObject
+{
+	static TinyHost CreateHost ()
+	{
+		string path = Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "web");
+		string bin = Path.Combine (path, "bin");
+		string asm = Path.GetFileName (typeof (TinyHost).Assembly.Location);
+
+		Directory.CreateDirectory (bin);
+		File.Copy (asm, Path.Combine (bin, asm), true);
+
+		return (TinyHost) ApplicationHost.CreateApplicationHost (
+			typeof (TinyHost), "/", path);
+	}
+
+	public void Execute (string page, TextWriter tw)
+	{
+		SimpleWorkerRequest req = new SimpleWorkerRequest (
+			page, "", tw);
+		HttpRuntime.ProcessRequest (req);
+	}
+
+	static int Main ()
+	{
+		TinyHost h = CreateHost ();
+		StringWriter sw = new StringWriter ();
+
+		h.Execute ("albums" + Path.DirectorySeparatorChar + "3.aspx", sw);
+		string result = sw.ToString ();
+		if (result.IndexOf ("<title>albumListing</title>") == -1) {
+			Console.WriteLine (result);
+			return 1;
+		}
+		return 0;
+	}
+}
---
+++ standalone/bug67892/web/AlbumListing.aspx.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace ServerTransfer
+{
+	public class AlbumListing : Page
+	{
+		void Page_Load(object sender, System.EventArgs e)
+		{
+		}
+
+		override protected void OnInit(EventArgs e)
+		{
+			InitializeComponent();
+			base.OnInit(e);
+		}
+
+		private void InitializeComponent()
+		{
+			Load += new EventHandler(Page_Load);
+		}
+	}
+}
