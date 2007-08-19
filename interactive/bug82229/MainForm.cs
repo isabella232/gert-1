8785e7c0a97a30b67cfaf939689b1b061cb2c2af
Gert Driesen <drieseng@users.sourceforge.net>
2007-08-19 11:36:13 +0000

    Added test for bug #82229.
    svn path=/trunk/gert/; revision=84386

--- interactive/bug82229/MainForm.cs
+++ interactive/bug82229/MainForm.cs
@@ -21,20 +21,6 @@ public class MainForm : Form
 		_tabGeneral.Text = "General";
 		_tabControl.Controls.Add (_tabGeneral);
 		// 
-		// _tabLookAndFeel
-		// 
-		_tabLookAndFeel = new TabPage ();
-		_tabLookAndFeel.TabIndex = 4;
-		_tabLookAndFeel.Text = "Look And Feel";
-		_tabControl.Controls.Add (_tabLookAndFeel);
-		// 
-		// _tabNetwork
-		// 
-		_tabNetwork = new TabPage ();
-		_tabNetwork.TabIndex = 2;
-		_tabNetwork.Text = "Network";
-		_tabControl.Controls.Add (_tabNetwork);
-		// 
 		// _tabAlerts
 		// 
 		_tabAlerts = new TabPage ();
@@ -42,26 +28,12 @@ public class MainForm : Form
 		_tabAlerts.Text = "Alerts";
 		_tabControl.Controls.Add (_tabAlerts);
 		// 
-		// _tabUpdates
-		// 
-		_tabUpdates = new TabPage ();
-		_tabUpdates.TabIndex = 3;
-		_tabUpdates.Text = "Updates";
-		_tabControl.Controls.Add (_tabUpdates);
-		// 
-		// _tabCalendar
-		// 
-		_tabCalendar = new TabPage ();
-		_tabCalendar.TabIndex = 5;
-		_tabCalendar.Text = "Calendar";
-		_tabControl.Controls.Add (_tabCalendar);
-		// 
 		// MainForm
 		// 
-		ClientSize = new Size (400, 100);
-		Location = new Point (200, 100);
+		ClientSize = new Size (300, 100);
+		Location = new Point (250, 100);
 		StartPosition = FormStartPosition.Manual;
-		Text = "bug #82471";
+		Text = "bug #82229";
 		Load += new EventHandler (MainForm_Load);
 	}
 
@@ -80,30 +52,6 @@ public class MainForm : Form
 	private System.Windows.Forms.TabControl _tabControl;
 	private System.Windows.Forms.TabPage _tabGeneral;
 	private System.Windows.Forms.TabPage _tabAlerts;
-	private System.Windows.Forms.TabPage _tabNetwork;
-	private System.Windows.Forms.TabPage _tabUpdates;
-	private System.Windows.Forms.TabPage _tabLookAndFeel;
-	private System.Windows.Forms.TabPage _tabCalendar;
-}
-
-public class ChildForm : Form
-{
-	public ChildForm ()
-	{
-		// 
-		// ChildForm
-		// 
-		ClientSize = new Size (300, 90);
-		Location = new Point (250, 250);
-		StartPosition = FormStartPosition.Manual;
-		Text = "Child";
-		Load += new EventHandler (ChildForm_Load);
-	}
-
-	void ChildForm_Load (object sender, EventArgs e)
-	{
-		Visible = false;
-	}
 }
 
 public class InstructionsForm : Form
@@ -124,14 +72,9 @@ public class InstructionsForm : Form
 		_bugDescriptionText1.Multiline = true;
 		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
 			"Expected result on start-up:{0}{0}" +
-			"1. The TabControl contains the following tabs:{0}{0}" +
-			"   * General{0}" +
-			"   * Look And Feel{0}" +
-			"   * Network{0}" +
-			"   * Alerts{0}" +
-			"   * Updates{0}" +
-			"   * Calendar{0}{0}" +
-			"2. The text on each leaf is fully visible.",
+			"1. The General tab is selected.{0}{0}" +
+			"2. The left border of the General leaf is aligned " +
+			"with the left border of the TabControl.",
 			Environment.NewLine);
 		// 
 		// _tabPage1
@@ -141,15 +84,36 @@ public class InstructionsForm : Form
 		_tabPage1.Controls.Add (_bugDescriptionText1);
 		_tabControl.Controls.Add (_tabPage1);
 		// 
+		// _bugDescriptionText2
+		// 
+		_bugDescriptionText2 = new TextBox ();
+		_bugDescriptionText2.Dock = DockStyle.Fill;
+		_bugDescriptionText2.Multiline = true;
+		_bugDescriptionText2.Text = string.Format (CultureInfo.InvariantCulture,
+			"Expected result on start-up:{0}{0}" +
+			"1. Select the Alerts tab.{0}{0}" +
+			"2. The left border of the General leaf is 2 pixels " +
+			"to the right of the left border of the TabControl.",
+			Environment.NewLine);
+		// 
+		// _tabPage2
+		// 
+		_tabPage2 = new TabPage ();
+		_tabPage2.Text = "#2";
+		_tabPage2.Controls.Add (_bugDescriptionText2);
+		_tabControl.Controls.Add (_tabPage2);
+		// 
 		// InstructionsForm
 		// 
-		ClientSize = new Size (300, 210);
-		Location = new Point (650, 100);
+		ClientSize = new Size (300, 130);
+		Location = new Point (600, 100);
 		StartPosition = FormStartPosition.Manual;
-		Text = "Instructions - bug #82471";
+		Text = "Instructions - bug #82229";
 	}
 
 	private TextBox _bugDescriptionText1;
+	private TextBox _bugDescriptionText2;
 	private TabControl _tabControl;
 	private TabPage _tabPage1;
+	private TabPage _tabPage2;
 }
