4b28465a20d1dda679799d54b4f53b5b506ee1d1
Gert Driesen <drieseng@users.sourceforge.net>
2007-08-29 14:44:07 +0000

    Added test for bug #82620 and bug #82622.
    svn path=/trunk/gert/; revision=84999

---
+++ standalone/bug82622/test.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+public class Test
+{
+	static void Main ()
+	{
+		Hashtable ht = new Hashtable ();
+		ht ["a"] = Run ("Appointments");
+	}
+
+	public static void Run (string unit)
+	{
+		return null;
+	}
+}
