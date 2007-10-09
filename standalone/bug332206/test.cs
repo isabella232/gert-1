c2745327cfdeee5cb2d06ea4b907b1856a6704a2
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-09 17:45:08 +0000

    Added test for bug #332206.
    svn path=/trunk/gert/; revision=87205

---
+++ standalone/bug332206/test.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading;
+
+class Program
+{
+	static int _counter;
+
+	static void Main ()
+	{
+		new Timer (new TimerCallback (tmrThreadingTimer_TimerCallback), null, 0, 1000);
+		while (_counter < 5) {
+		}
+	}
+
+	static void tmrThreadingTimer_TimerCallback (object state)
+	{
+		_counter++;
+		throw new Exception ("Booom");
+	}
+}
