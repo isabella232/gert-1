e0747b3f5e58764d293eaab0aad833f32062512b
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-11 18:16:21 +0000

    Improved test by jpobst.
    svn path=/trunk/gert/; revision=87353

--- standalone/bug332409/test.cs
+++ standalone/bug332409/test.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Windows.Forms;
 
 class Program
 {
-	static void Main ()
+	static int Main ()
 	{
-		Form a = new Form ();
-		a.Show ();
+		Form form = new Form ();
+		form.KeyDown += new KeyEventHandler (Form_KeyDown);
+		form.Show ();
 		SendKeys.SendWait ("a");
+
+		if (_keyDown == null)
+			return 1;
+		if (_keyDown != "A")
+			return 2;
+		return 0;
 	}
+
+	static void Form_KeyDown (object sender, KeyEventArgs e)
+	{
+		_keyDown = e.KeyData.ToString ();
+	}
+
+	private static string _keyDown;
 }
