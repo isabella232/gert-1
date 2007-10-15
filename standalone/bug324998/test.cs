611d8ae6326bdc9a860d1fbbfddd31a8d5590034
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-15 13:32:18 +0000

    Added test for bug #324998.
    svn path=/trunk/gert/; revision=87510

---
+++ standalone/bug324998/test.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Reflection;
+
+static class Program
+{
+	private const BindingFlags BINDING_FLAGS
+		= BindingFlags.Public | BindingFlags.NonPublic
+		| BindingFlags.Instance | BindingFlags.Static
+		| BindingFlags.IgnoreCase;
+
+	[STAThread]
+	static int Main ()
+	{
+		Type tB = typeof (B);
+		try {
+			tB.GetMethod ("f", BINDING_FLAGS);
+			return 1;
+		} catch (AmbiguousMatchException) {
+			return 0;
+		}
+	}
+}
+
+class A
+{
+	public virtual void f (int i1, int i2) { }
+	public virtual void f (int i1, int i2, bool b) { }
+}
+
+class B : A
+{
+	public override void f (int i1, int i2, bool b) { }
+}
