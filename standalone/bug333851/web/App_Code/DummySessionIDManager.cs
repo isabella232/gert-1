b82abceb12b0f68f13ee82125f009bf5223ba136
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-15 12:46:47 +0000

    Added test for bug #333851.
    svn path=/trunk/gert/; revision=87503

---
+++ standalone/bug333851/web/App_Code/DummySessionIDManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Test {
+	public class DummySessionIDManager : SessionIDManager {
+		public override string CreateSessionID (HttpContext context)
+		{
+			throw new Exception ("Exception from DummySessionIDManager");
+		}
+	}
+}
