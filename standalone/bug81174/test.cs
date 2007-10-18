7f734d3e1081cb171a1b60143d7cd9e06a2f6acc
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-18 17:13:15 +0000

    Use XSP instead of hosting the ASP.NET runtime ourselves.
    svn path=/trunk/gert/; revision=87771

--- standalone/bug81174/test.cs
+++ standalone/bug81174/test.cs
@@ -1,68 +1,62 @@
 using System;
 using System.IO;
-using System.Web;
-using System.Web.Hosting;
+using System.Net;
+using System.Text;
 
-class TinyHost : MarshalByRefObject
+class Program
 {
-	static TinyHost CreateHost ()
-	{
-		string path = Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "web");
-		string bin = Path.Combine (path, "bin");
-		string asm = Path.GetFileName (typeof (TinyHost).Assembly.Location);
-
-		Directory.CreateDirectory (bin);
-		File.Copy (asm, Path.Combine (bin, asm), true);
-
-		return (TinyHost) ApplicationHost.CreateApplicationHost (
-			typeof (TinyHost), "/", path);
-	}
-
-	public void Execute (string page, TextWriter tw)
-	{
-		SimpleWorkerRequest req = new SimpleWorkerRequest (
-			page, "", tw);
-		HttpRuntime.ProcessRequest (req);
-	}
-
 	static int Main ()
 	{
-		TinyHost h = CreateHost ();
-		StringWriter sw = new StringWriter ();
-		h.Execute ("Create.aspx", sw);
-		string result = sw.ToString ();
-		if (result.IndexOf ("Een titel") == -1) {
-			Console.WriteLine (result);
-			return 1;
-		}
-		if (result.IndexOf ("Hoofding") == -1) {
-			Console.WriteLine (result);
-			return 2;
-		}
+		HttpWebRequest request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Create.aspx");
+		request.Method = "GET";
 
-		sw.GetStringBuilder ().Length = 0;
-		h.Execute ("Default.aspx", sw);
-		result = sw.ToString ();
-		if (result.IndexOf ("Noot") == -1) {
-			Console.WriteLine (result);
-			return 3;
+		HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+		using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+			string result = sr.ReadToEnd ();
+			if (result.IndexOf ("Een titel") == -1) {
+				Console.WriteLine (result);
+				return 1;
+			}
+			if (result.IndexOf ("Hoofding") == -1) {
+				Console.WriteLine (result);
+				return 2;
+			}
 		}
-		if (result.IndexOf ("Inhoudstafel") == -1) {
-			Console.WriteLine (result);
-			return 4;
-		}
+		response.Close ();
 
-		sw.GetStringBuilder ().Length = 0;
-		h.Execute ("Sub" + Path.DirectorySeparatorChar + "Default.aspx", sw);
-		result = sw.ToString ();
-		if (result.IndexOf ("Noot") == -1) {
-			Console.WriteLine (result);
-			return 5;
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Default.aspx");
+		request.Method = "GET";
+
+		response = (HttpWebResponse) request.GetResponse ();
+		using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+			string result = sr.ReadToEnd ();
+			if (result.IndexOf ("Noot") == -1) {
+				Console.WriteLine (result);
+				return 3;
+			}
+			if (result.IndexOf ("Inhoudstafel") == -1) {
+				Console.WriteLine (result);
+				return 4;
+			}
 		}
-		if (result.IndexOf ("Inhoudsopgave") == -1) {
-			Console.WriteLine (result);
-			return 6;
+		response.Close ();
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Sub/Default.aspx");
+		request.Method = "GET";
+
+		response = (HttpWebResponse) request.GetResponse ();
+		using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+			string result = sr.ReadToEnd ();
+			if (result.IndexOf ("Noot") == -1) {
+				Console.WriteLine (result);
+				return 5;
+			}
+			if (result.IndexOf ("Inhoudsopgave") == -1) {
+				Console.WriteLine (result);
+				return 6;
+			}
 		}
+		response.Close ();
 
 		return 0;
 	}
