1931d7bd49a386c8c8ec54e0d30fa818e509e366
Gert Driesen <drieseng@users.sourceforge.net>
2007-10-31 23:23:38 +0000

    Added IsFalse, IsTrue and AreEqual overload.
    svn path=/trunk/gert/; revision=88616

--- build/common/Assert.cs
+++ build/common/Assert.cs
@@ -3,6 +3,16 @@ using System.Globalization;
 
 class Assert
 {
+	public static void AreEqual (string x, string y, string msg)
+	{
+		if (x == null && y == null)
+			return;
+		if ((x == null || y == null) || !x.Equals (y))
+			throw new Exception (string.Format (CultureInfo.InvariantCulture,
+				"Expected: {0}, but was: {1}. {2}",
+				x, y, msg));
+	}
+
 	public static void AreEqual (object x, object y, string msg)
 	{
 		if (x == null && y == null)
@@ -16,6 +26,18 @@ class Assert
 	public static void Fail (string msg)
 	{
 		throw new Exception (msg);
+	}
+
+	public static void IsFalse (bool value, string msg)
+	{
+		if (value)
+			throw new Exception (msg);
+	}
+
+	public static void IsTrue (bool value, string msg)
+	{
+		if (!value)
+			throw new Exception (msg);
 	}
 
 	public static void IsNotNull (object value, string msg)
