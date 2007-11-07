b57286ee8719122242f3afe8473f49d22ccd95f0
Gert Driesen <drieseng@users.sourceforge.net>
2007-11-07 18:51:39 +0000

    Added test for bug #339934.
    svn path=/trunk/gert/; revision=89146

---
+++ standalone/bug339934/test.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+
+class MainTestCase
+{
+	static int Main ()
+	{
+		XmlSchema schema = XmlSchema.Read (new XmlTextReader ("schema.xsd"), null);
+
+#if NET_2_0
+		XmlReaderSettings settings = new XmlReaderSettings ();
+		settings.ValidationType = ValidationType.Schema;
+		settings.Schemas.Add (schema);
+
+		XmlReader reader = XmlReader.Create (new StringReader (xml), settings);
+		try {
+			while (reader.Read ()) ;
+			return 1;
+		} catch (XmlSchemaValidationException) {
+		} finally {
+			reader.Close ();
+		}
+#endif
+
+		XmlValidatingReader validator = new XmlValidatingReader (xml, XmlNodeType.Document, null);
+		validator.ValidationType = ValidationType.Schema;
+		validator.Schemas.Add (schema);
+		try {
+			while (validator.Read ()) ;
+			return 2;
+		} catch (XmlSchemaException) {
+		} finally {
+			validator.Close ();
+		}
+
+		return 0;
+	}
+
+	static string xml = @"<test mode=""NOT A ENUMERATION VALUE""></test>";
+}
