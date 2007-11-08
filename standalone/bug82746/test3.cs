c7d55a47c0757eb567925634cd07e64389df5686
Gert Driesen <drieseng@users.sourceforge.net>
2007-11-08 20:22:12 +0000

    Improve tests due to bug #339787.
    svn path=/trunk/gert/; revision=89248

--- standalone/bug82746/test3.cs
+++ standalone/bug82746/test3.cs
@@ -2,14 +2,13 @@ using System.Reflection;
 using System.Windows.Forms;
 
 [assembly: AssemblyFileVersion ("4.3.2.1")]
-[assembly: AssemblyInformationalVersion ("1.2.3.4")]
 [assembly: AssemblyVersion ("5.6.7.8")]
 
 class Program
 {
 	static int Main ()
 	{
-		if (Application.ProductVersion != "1.2.3.4")
+		if (Application.ProductVersion != "4.3.2.1")
 			return 1;
 		return 0;
 	}
