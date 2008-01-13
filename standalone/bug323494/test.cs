405ca4fa4bcf4fb226d18334c9044dcd7eae219c
Gert Driesen <drieseng@users.sourceforge.net>
2008-01-13 13:54:32 +0000

    Added test for bug #323494.
    svn path=/trunk/gert/; revision=92796

---
+++ standalone/bug323494/test.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+	static int Main ()
+	{
+		Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+		Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+		HttpWebRequest request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Default.aspx");
+		request.Method = "GET";
+
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+				if (result.IndexOf ("<a id=\"HyperLink1\">Resource Based Name</a>") == -1) {
+					Console.WriteLine (result);
+					return 1;
+				}
+			}
+			response.Close ();
+		} catch (WebException ex) {
+			if (ex.Response != null) {
+				StreamReader sr = new StreamReader (ex.Response.GetResponseStream ());
+				Console.WriteLine (sr.ReadToEnd ());
+			}
+			return 2;
+		}
+
+		return 0;
+	}
+}
