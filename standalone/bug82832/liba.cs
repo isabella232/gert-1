f93f41946156e2485f4f30b72aa3f20ca51432ab
Gert Driesen <drieseng@users.sourceforge.net>
2008-01-17 20:23:54 +0000

    Improved test.
    svn path=/trunk/gert/; revision=93199

---
+++ standalone/bug82832/liba.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion ("9.7.5.3")]
+[assembly: AssemblyFileVersion ("3.2.3.2")]
+[assembly: AssemblyProduct ("lib8")]
+[assembly: AssemblyTitle ("lib8 title")]
+[assembly: AssemblyInformationalVersion ("0.2.4.0")]
+[assembly: AssemblyDescription ("lib8 description")]
+[assembly: AssemblyCopyright ("lib8 copyright")]
+[assembly: AssemblyCompany ("lib8 company")]
+[assembly: AssemblyTrademark ("lib8 trademark")]
+
+class Foo
+{
+}
--- standalone/bug82832/test.cs
+++ standalone/bug82832/test.cs
@@ -7,30 +7,58 @@ using System.Reflection.Emit;
 
 class Program
 {
-	static void Main ()
+	static int Main (string [] args)
 	{
-		Test1 ();
-		Test2 ();
-		Test3 ();
-		Test4 ();
-		Test5 ();
-		Test6 ();
-		Test7 ();
+		if (args.Length == 0) {
+			Console.Error.WriteLine ("Please specify the action.");
+			return 1;
+		}
+
+		switch (args [0]) {
+		case "create":
+			Create1 ();
+			Create2 ();
+			Create3 ();
+			Create4 ();
+			Create5 ();
+			Create6 ();
+			Create7 ();
+			break;
+		case "verify":
+			Verify1 ();
+			Verify2 ();
+			Verify3 ();
+			Verify4 ();
+			Verify5 ();
+			Verify7 ();
+			Verify8 ();
+			Verify9 ();
+			Verify10 ();
+			break;
+		default:
+			Console.Error.WriteLine ("Unsupported action.");
+			return 1;
+		}
+
+		return 0;
 	}
 
-	static void Test1 ()
+	static void Create1 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib1";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
 			AppDomain.CurrentDomain.BaseDirectory);
 		ab.DefineVersionInfoResource ("BBB", "2.0", "CCC", "DDD", "EEE");
-		ab.Save ("lib.dll");
+		ab.Save ("lib1.dll");
+	}
+
+	static void Verify1 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib1.dll");
 
 		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
 		Assert.AreEqual ("CCC", fvi.CompanyName, "#A1");
@@ -43,23 +71,24 @@ class Program
 		Assert.AreEqual (0, fvi.FileMajorPart, "#A8");
 		Assert.AreEqual (0, fvi.FileMinorPart, "#A9");
 		Assert.AreEqual (0, fvi.FilePrivatePart, "#A10");
-
-		File.Delete (assemblyFile);
 	}
 
-	static void Test2 ()
+	static void Create2 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib2";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
 			AppDomain.CurrentDomain.BaseDirectory);
 		ab.DefineVersionInfoResource (null, null, null, null, null);
-		ab.Save ("lib.dll");
+		ab.Save ("lib2.dll");
+	}
+
+	static void Verify2 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib2.dll");
 
 		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
 		Assert.IsNotNull (fvi.CompanyName, "#B1");
@@ -77,17 +106,12 @@ class Program
 		Assert.AreEqual (0, fvi.FileMajorPart, "#B13");
 		Assert.AreEqual (0, fvi.FileMinorPart, "#B14");
 		Assert.AreEqual (0, fvi.FilePrivatePart, "#B15");
-
-		File.Delete (assemblyFile);
 	}
 
-	static void Test3 ()
+	static void Create3 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib3";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
@@ -133,7 +157,13 @@ class Program
 		ab.SetCustomAttribute (cab);
 
 		ab.DefineVersionInfoResource ();
-		ab.Save ("lib.dll");
+		ab.Save ("lib3.dll");
+	}
+
+	static void Verify3 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib3.dll");
 
 		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
 		Assert.AreEqual ("Mono Team", fvi.CompanyName, "#C1");
@@ -146,17 +176,12 @@ class Program
 		Assert.AreEqual (2, fvi.FileMajorPart, "#C8");
 		Assert.AreEqual (4, fvi.FileMinorPart, "#C9");
 		Assert.AreEqual (8, fvi.FilePrivatePart, "#C10");
-
-		File.Delete (assemblyFile);
 	}
 
-	static void Test4 ()
+	static void Create4 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib4";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
@@ -202,7 +227,13 @@ class Program
 		ab.SetCustomAttribute (cab);
 
 		ab.DefineVersionInfoResource ("AAA", "2.0", "BBB", "CCC", "DDD");
-		ab.Save ("lib.dll");
+		ab.Save ("lib4.dll");
+	}
+
+	static void Verify4 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib4.dll");
 
 		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
 		Assert.AreEqual ("BBB", fvi.CompanyName, "#D1");
@@ -215,23 +246,24 @@ class Program
 		Assert.AreEqual (0, fvi.FileMajorPart, "#D8");
 		Assert.AreEqual (0, fvi.FileMinorPart, "#D9");
 		Assert.AreEqual (0, fvi.FilePrivatePart, "#D10");
-
-		File.Delete (assemblyFile);
 	}
 
-	static void Test5 ()
+	static void Create5 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib5";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
 			AppDomain.CurrentDomain.BaseDirectory);
 		ab.DefineVersionInfoResource ();
-		ab.Save ("lib.dll");
+		ab.Save ("lib5.dll");
+	}
+
+	static void Verify5 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib5.dll");
 
 		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
 		Assert.IsNotNull (fvi.CompanyName, "#E1");
@@ -249,17 +281,12 @@ class Program
 		Assert.AreEqual (0, fvi.FileMajorPart, "#E13");
 		Assert.AreEqual (0, fvi.FileMinorPart, "#E14");
 		Assert.AreEqual (0, fvi.FilePrivatePart, "#E15");
-
-		File.Delete (assemblyFile);
 	}
 
-	static void Test6 ()
+	static void Create6 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib6";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
@@ -275,17 +302,12 @@ class Program
 			Assert.IsNotNull (ex.Message, "#F4");
 			Assert.IsNull (ex.ParamName, "#F4");
 		}
-
-		File.Delete (assemblyFile);
 	}
 
-	static void Test7 ()
+	static void Create7 ()
 	{
 		AssemblyName aname = new AssemblyName ();
-		aname.Name = "lib";
-
-		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
-			"lib.dll");
+		aname.Name = "lib7";
 
 		AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (
 			aname, AssemblyBuilderAccess.RunAndSave,
@@ -298,20 +320,123 @@ class Program
 		ab.SetCustomAttribute (cab);
 
 		ab.DefineVersionInfoResource ();
-		ab.Save ("lib.dll");
+		ab.Save ("lib7.dll");
+	}
+
+	static void Verify7 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib7.dll");
 
 		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
-		Assert.AreEqual (" ", fvi.CompanyName, "#F1");
-		Assert.AreEqual (" ", fvi.ProductName, "#F2");
-		Assert.AreEqual (" ", fvi.ProductVersion, "#F3");
-		Assert.AreEqual (" ", fvi.LegalCopyright, "#F4");
-		Assert.AreEqual (" ", fvi.LegalTrademarks, "#F5");
-		Assert.AreEqual ("0.0.0.0", fvi.FileVersion, "#F6");
-		Assert.AreEqual (0, fvi.FileBuildPart, "#F7");
-		Assert.AreEqual (0, fvi.FileMajorPart, "#F8");
-		Assert.AreEqual (0, fvi.FileMinorPart, "#F9");
-		Assert.AreEqual (0, fvi.FilePrivatePart, "#F10");
+		Assert.AreEqual (" ", fvi.CompanyName, "#G1");
+		Assert.AreEqual (" ", fvi.ProductName, "#G2");
+		Assert.AreEqual (" ", fvi.ProductVersion, "#G3");
+		Assert.AreEqual (" ", fvi.LegalCopyright, "#G4");
+		Assert.AreEqual (" ", fvi.LegalTrademarks, "#G5");
+		Assert.AreEqual ("0.0.0.0", fvi.FileVersion, "#G6");
+		Assert.AreEqual (0, fvi.FileBuildPart, "#G7");
+		Assert.AreEqual (0, fvi.FileMajorPart, "#G8");
+		Assert.AreEqual (0, fvi.FileMinorPart, "#G9");
+		Assert.AreEqual (0, fvi.FilePrivatePart, "#G10");
+	}
 
-		File.Delete (assemblyFile);
+	static void Verify8 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib8a.dll");
+
+		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
+		Assert.AreEqual ("Mono Team", fvi.CompanyName, "#H1a");
+		Assert.AreEqual ("Mono Runtime", fvi.ProductName, "#H2a");
+		Assert.AreEqual ("4,2,1,7", fvi.ProductVersion, "#H3a");
+		Assert.AreEqual ("Copyright 2007 Mono Hackers", fvi.LegalCopyright, "#H4a");
+		Assert.AreEqual ("Registered to All", fvi.LegalTrademarks, "#H5a");
+		Assert.AreEqual ("1.2.3.4", fvi.FileVersion, "#H6a");
+		Assert.AreEqual (6, fvi.FileBuildPart, "#H7a");
+		Assert.AreEqual (2, fvi.FileMajorPart, "#H8a");
+		Assert.AreEqual (4, fvi.FileMinorPart, "#H9a");
+		Assert.AreEqual (8, fvi.FilePrivatePart, "#H10a");
+
+		assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib8b.dll");
+
+		fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
+		Assert.AreEqual ("Mono Team", fvi.CompanyName, "#H1b");
+		Assert.AreEqual ("Mono Runtime", fvi.ProductName, "#H2b");
+		Assert.AreEqual ("4,2,1,7", fvi.ProductVersion, "#H3b");
+		Assert.AreEqual ("Copyright 2007 Mono Hackers", fvi.LegalCopyright, "#H4b");
+		Assert.AreEqual ("Registered to All", fvi.LegalTrademarks, "#H5b");
+		Assert.AreEqual ("1.2.3.4", fvi.FileVersion, "#H6b");
+		Assert.AreEqual (6, fvi.FileBuildPart, "#H7b");
+		Assert.AreEqual (2, fvi.FileMajorPart, "#H8b");
+		Assert.AreEqual (4, fvi.FileMinorPart, "#H9b");
+		Assert.AreEqual (8, fvi.FilePrivatePart, "#H10b");
+	}
+
+	static void Verify9 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib9a.dll");
+
+		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
+		Assert.AreEqual ("", fvi.CompanyName, "#I1a");
+		Assert.AreEqual ("", fvi.ProductName, "#I2a");
+		Assert.AreEqual ("", fvi.ProductVersion, "#I3a");
+		Assert.AreEqual ("", fvi.LegalCopyright, "#I4a");
+		Assert.AreEqual ("", fvi.LegalTrademarks, "#I5a");
+		Assert.AreEqual ("", fvi.FileVersion, "#I6a");
+		Assert.AreEqual (6, fvi.FileBuildPart, "#I7a");
+		Assert.AreEqual (2, fvi.FileMajorPart, "#I8a");
+		Assert.AreEqual (4, fvi.FileMinorPart, "#I9a");
+		Assert.AreEqual (8, fvi.FilePrivatePart, "#I10a");
+
+		assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib9b.dll");
+
+		fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
+		Assert.AreEqual ("", fvi.CompanyName, "#I1b");
+		Assert.AreEqual ("", fvi.ProductName, "#I2b");
+		Assert.AreEqual ("", fvi.ProductVersion, "#I3b");
+		Assert.AreEqual ("", fvi.LegalCopyright, "#I4b");
+		Assert.AreEqual ("", fvi.LegalTrademarks, "#I5b");
+		Assert.AreEqual ("", fvi.FileVersion, "#I6b");
+		Assert.AreEqual (6, fvi.FileBuildPart, "#I7b");
+		Assert.AreEqual (2, fvi.FileMajorPart, "#I8b");
+		Assert.AreEqual (4, fvi.FileMinorPart, "#I9b");
+		Assert.AreEqual (8, fvi.FilePrivatePart, "#I10b");
+	}
+
+	static void Verify10 ()
+	{
+		string assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib10a.dll");
+
+		FileVersionInfo fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
+		Assert.AreEqual ("", fvi.CompanyName, "#J1a");
+		Assert.AreEqual ("", fvi.ProductName, "#J2a");
+		Assert.AreEqual ("", fvi.ProductVersion, "#J3a");
+		Assert.AreEqual ("", fvi.LegalCopyright, "#J4a");
+		Assert.AreEqual ("", fvi.LegalTrademarks, "#J5a");
+		Assert.AreEqual ("", fvi.FileVersion, "#J6a");
+		Assert.AreEqual (6, fvi.FileBuildPart, "#J7a");
+		Assert.AreEqual (2, fvi.FileMajorPart, "#J8a");
+		Assert.AreEqual (4, fvi.FileMinorPart, "#J9a");
+		Assert.AreEqual (8, fvi.FilePrivatePart, "#J10a");
+
+		assemblyFile = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+			"lib10b.dll");
+
+		fvi = FileVersionInfo.GetVersionInfo (assemblyFile);
+		Assert.AreEqual ("", fvi.CompanyName, "#J1b");
+		Assert.AreEqual ("", fvi.ProductName, "#J2b");
+		Assert.AreEqual ("", fvi.ProductVersion, "#J3b");
+		Assert.AreEqual ("", fvi.LegalCopyright, "#J4b");
+		Assert.AreEqual ("", fvi.LegalTrademarks, "#J5b");
+		Assert.AreEqual ("", fvi.FileVersion, "#J6b");
+		Assert.AreEqual (6, fvi.FileBuildPart, "#J7b");
+		Assert.AreEqual (2, fvi.FileMajorPart, "#J8b");
+		Assert.AreEqual (4, fvi.FileMinorPart, "#J9b");
+		Assert.AreEqual (8, fvi.FilePrivatePart, "#J10b");
 	}
 }
