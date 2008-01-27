ffb16f4b0b75a2875fc87aa622927df769daf92f
Gert Driesen <drieseng@users.sourceforge.net>
2008-01-27 09:02:47 +0000

    Added test for bug #356544.
    svn path=/trunk/gert/; revision=94037

---
+++ standalone/bug356544/test.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Reflection;
+
+namespace Mono
+{
+	class Program
+	{
+		static void Main ()
+		{
+			Type t = typeof (Foo);
+			Type [] interfaces = t.GetInterfaces ();
+			Assert.AreEqual (1, interfaces.Length, "#A1");
+			Assert.AreEqual ("Mono.Bar+IMarshal", interfaces [0].FullName, "#A2");
+
+			MethodInfo [] methods = t.GetMethods (BindingFlags.DeclaredOnly |
+				BindingFlags.NonPublic | BindingFlags.Public |
+				BindingFlags.Instance);
+
+			Assert.AreEqual (1, methods.Length, "#B1");
+			Assert.AreEqual ("Mono.Bar+IMarshal.Release", methods [0].Name, "#B2");
+		}
+	}
+
+	class Foo : Bar.IMarshal
+	{
+		void Bar.IMarshal.Release ()
+		{
+		}
+	}
+
+	class Bar
+	{
+		public interface IMarshal
+		{
+			void Release ();
+		}
+	}
+}
