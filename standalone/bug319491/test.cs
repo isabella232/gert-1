f42e34e23ed6a7d22239be92b852785f10100620
Gert Driesen <drieseng@users.sourceforge.net>
2008-01-29 19:28:13 +0000

    Report actual error.
    svn path=/trunk/gert/; revision=94298

--- standalone/bug319491/test.cs
+++ standalone/bug319491/test.cs
@@ -10,15 +10,23 @@ class Program
 		HttpWebRequest request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Default.aspx");
 		request.Method = "GET";
 
-		HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
-		using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
-			string result = sr.ReadToEnd ();
-			if (result.IndexOf ("class=\"red\" class=\"green\"") == -1) {
-				Console.WriteLine (result);
-				return 1;
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+				if (result.IndexOf ("class=\"red\" class=\"green\"") == -1) {
+					Console.WriteLine (result);
+					return 1;
+				}
 			}
+			response.Close ();
+		} catch (WebException ex) {
+			if (ex.Response != null) {
+				StreamReader sr = new StreamReader (ex.Response.GetResponseStream ());
+				Console.WriteLine (sr.ReadToEnd ());
+			}
+			return 2;
 		}
-		response.Close ();
 
 		return 0;
 	}
