47d64f9a7b3e86e122eb0b2e982fd0ae6de5c643
Gert Driesen <drieseng@users.sourceforge.net>
2008-02-02 13:33:51 +0000

    Added test for bug #343966.
    svn path=/trunk/gert/; revision=94620

---
+++ interactive/bug343966/MainForm.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+class MainForm : Form
+{
+	public MainForm ()
+	{
+		// 
+		// _dateTimePicker
+		// 
+		_dateTimePicker = new DateTimePicker ();
+		Controls.Add (_dateTimePicker);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (300, 60);
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #343966";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private DateTimePicker _dateTimePicker;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Steps to execute:{0}{0}" +
+			"1. Click the dropdown arrow of the DateTimePicker.{0}{0}" +
+			"2. Click it again.{0}{0}" +
+			"Expected result:{0}{0}" +
+			"1. On step 1, the DateTimerPicker is dropped down.{0}{0}" +
+			"2. On step 2, the DateTimerPicker is closed.",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (300, 190);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #343966";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+}
+
