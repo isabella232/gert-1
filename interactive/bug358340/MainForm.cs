23a0f875dbe6bfb136ee244087f96e9090d12e58
Gert Driesen <drieseng@users.sourceforge.net>
2008-02-02 13:49:24 +0000

    Added test for bug #358340.
    svn path=/trunk/gert/; revision=94624

---
+++ interactive/bug358340/MainForm.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+class MainForm : Form
+{
+	public MainForm ()
+	{
+		// 
+		// _dateTimePicker
+		// 
+		_dateTimePicker = new DateTimePicker ();
+		_dateTimePicker.Location = new Point (55, 20);
+		Controls.Add (_dateTimePicker);
+		// 
+		// MainForm
+		// 
+		ClientSize = new Size (300, 60);
+		Location = new Point (250, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "bug #358340";
+		Load += new EventHandler (MainForm_Load);
+	}
+
+	[STAThread]
+	static void Main ()
+	{
+		Application.Run (new MainForm ());
+	}
+
+	void MainForm_Load (object sender, EventArgs e)
+	{
+		InstructionsForm instructionsForm = new InstructionsForm ();
+		instructionsForm.Show ();
+	}
+
+	private DateTimePicker _dateTimePicker;
+}
+
+public class InstructionsForm : Form
+{
+	public InstructionsForm ()
+	{
+		// 
+		// _tabControl
+		// 
+		_tabControl = new TabControl ();
+		_tabControl.Dock = DockStyle.Fill;
+		Controls.Add (_tabControl);
+		// 
+		// _bugDescriptionText1
+		// 
+		_bugDescriptionText1 = new TextBox ();
+		_bugDescriptionText1.Dock = DockStyle.Fill;
+		_bugDescriptionText1.Multiline = true;
+		_bugDescriptionText1.Text = string.Format (CultureInfo.InvariantCulture,
+			"Steps to execute:{0}{0}" +
+			"1. Press the Alt+F4 key.{0}{0}" +
+			"Expected result:{0}{0}" +
+			"1. The application exits.",
+			Environment.NewLine);
+		// 
+		// _tabPage1
+		// 
+		_tabPage1 = new TabPage ();
+		_tabPage1.Text = "#1";
+		_tabPage1.Controls.Add (_bugDescriptionText1);
+		_tabControl.Controls.Add (_tabPage1);
+		// 
+		// InstructionsForm
+		// 
+		ClientSize = new Size (300, 140);
+		Location = new Point (600, 100);
+		StartPosition = FormStartPosition.Manual;
+		Text = "Instructions - bug #358340";
+	}
+
+	private TextBox _bugDescriptionText1;
+	private TabControl _tabControl;
+	private TabPage _tabPage1;
+}
+
