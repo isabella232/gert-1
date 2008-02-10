788e2cb8289e12d09a2e98e6f676ee3a70463079
Gert Driesen <drieseng@users.sourceforge.net>
2008-02-10 17:27:31 +0000

    Test ToString too.
    svn path=/trunk/gert/; revision=95388

--- standalone/bug318231/test.cs
+++ standalone/bug318231/test.cs
@@ -9,7 +9,8 @@ class Program
 		string dir = AppDomain.CurrentDomain.BaseDirectory;
 
 		Assembly a = Assembly.LoadFrom (Path.Combine (dir, "lib.dll"));
-		Assert.AreEqual ("lib, Version=0.0.0.0, Culture=doesnot-exist, PublicKeyToken=null", a.FullName, "#A");
+		Assert.AreEqual ("lib, Version=0.0.0.0, Culture=doesnot-exist, PublicKeyToken=null", a.FullName, "#A1");
+		Assert.AreEqual ("lib, Version=0.0.0.0, Culture=doesnot-exist, PublicKeyToken=null", a.ToString (), "#A2");
 
 		try {
 			a.GetName ();
