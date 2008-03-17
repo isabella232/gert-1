e06d74e05c1114b69ef2919a05149a608d0c0f14
Gert Driesen <drieseng@users.sourceforge.net>
2008-03-17 18:37:27 +0000

    Allow test to run on unix (with gnome).
    svn path=/trunk/gert/; revision=98479

--- standalone/bug371567/test.cs
+++ standalone/bug371567/test.cs
@@ -7,9 +7,19 @@ class Program
 	static void Main ()
 	{
 		Process p = new Process ();
-		p.StartInfo.FileName = "explorer.exe";
+		p.StartInfo.FileName = RunningOnUnix ? "gnome-open" : "explorer.exe";
 		p.StartInfo.Arguments = "http://www.google.be#whatever";
 		p.Start ();
 		p.WaitForExit ();
+	}
+
+	static bool RunningOnUnix {
+		get {
+#if NET_2_0
+			return Environment.OSVersion.Platform == PlatformID.Unix;
+#else
+			return (int) Environment.OSVersion.Platform == 128;
+#endif
+		}
 	}
 }
