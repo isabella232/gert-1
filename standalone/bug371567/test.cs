fc7f93e09b66229f4d27a19b634b4e3edc1f3dd7
Gert Driesen <drieseng@users.sourceforge.net>
2008-03-17 18:47:46 +0000

    Improve test.
    svn path=/trunk/gert/; revision=98480

--- standalone/bug371567/test.cs
+++ standalone/bug371567/test.cs
@@ -7,19 +7,15 @@ class Program
 	static void Main ()
 	{
 		Process p = new Process ();
-		p.StartInfo.FileName = RunningOnUnix ? "gnome-open" : "explorer.exe";
+		p.StartInfo.FileName = "echo";
 		p.StartInfo.Arguments = "http://www.google.be#whatever";
+		p.StartInfo.RedirectStandardOutput = true;
+		p.StartInfo.UseShellExecute = false;
 		p.Start ();
 		p.WaitForExit ();
-	}
+		Assert.AreEqual (0, p.ExitCode, "#1");
 
-	static bool RunningOnUnix {
-		get {
-#if NET_2_0
-			return Environment.OSVersion.Platform == PlatformID.Unix;
-#else
-			return (int) Environment.OSVersion.Platform == 128;
-#endif
-		}
+		string result = p.StandardOutput.ReadToEnd ();
+		Assert.AreEqual ("http://www.google.be#whatever\n", result, "#2");
 	}
 }
