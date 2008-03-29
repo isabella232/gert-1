14d3b15f9e8a72b44d83c3ff22b31a3f546665ca
Gert Driesen <drieseng@users.sourceforge.net>
2008-03-29 12:11:11 +0000

    Added missing sources.
    svn path=/trunk/gert/; revision=99274

---
+++ standalone/bug82832/libc.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion ("9.7.5.3")]
+[assembly: AssemblyFileVersion ("3.2.3.2")]
+[assembly: AssemblyProduct ("libc")]
+[assembly: AssemblyTitle ("libc title")]
+[assembly: AssemblyInformationalVersion ("7.2.4.3")]
+[assembly: AssemblyDescription ("libc description")]
+[assembly: AssemblyCopyright ("libc copyright")]
+[assembly: AssemblyCompany ("libc company")]
+[assembly: AssemblyTrademark ("libc trademark")]
+[assembly: AssemblyCulture ("doesnotexist")]
+
+class Foo
+{
+}
---
+++ standalone/bug82832/libd.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion ("1.2.3")]
+[assembly: AssemblyFileVersion ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyTitle ("")]
+[assembly: AssemblyInformationalVersion ("")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyTrademark ("")]
+
+class Foo
+{
+}
