96e35609b0ca5ba3a8d67865b3dcad4920ade97b
Gert Driesen <drieseng@users.sourceforge.net>
2008-04-01 20:46:21 +0000

    Cleanup.
    svn path=/trunk/gert/; revision=99576

--- standalone/bug333851/web/App_Code/DummySessionIDManager.cs
+++ standalone/bug333851/web/App_Code/DummySessionIDManager.cs
@@ -2,8 +2,10 @@ using System;
 using System.Web;
 using System.Web.SessionState;
 
-namespace Test {
-	public class DummySessionIDManager : SessionIDManager {
+namespace Test
+{
+	public class DummySessionIDManager : SessionIDManager
+	{
 		public override string CreateSessionID (HttpContext context)
 		{
 			throw new Exception ("Exception from DummySessionIDManager");
