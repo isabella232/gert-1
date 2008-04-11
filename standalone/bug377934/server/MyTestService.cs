77bad9cda1d97e9c6863276536d8224f94c6c070
Gert Driesen <drieseng@users.sourceforge.net>
2008-04-11 12:56:28 +0000

    Cleanup.
    svn path=/trunk/gert/; revision=100426

--- standalone/bug377934/server/MyTestService.cs
+++ standalone/bug377934/server/MyTestService.cs
@@ -3,11 +3,7 @@ using System.Web.Services;
 
 namespace Mono.Web.Services
 {
-#if NET_2_0
 	[WebServiceBinding (ConformsTo = WsiProfiles.BasicProfile1_1, EmitConformanceClaims = true)]
-#else
-	[WebServiceBinding]
-#endif
 	public class MyTestService : WebService
 	{
 		[WebMethod]
