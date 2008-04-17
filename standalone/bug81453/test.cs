e98cc1cb62a55dd435c651e20776a7e24a8b827a
Gert Driesen <drieseng@users.sourceforge.net>
2008-04-17 17:19:05 +0000

    I fixed this a while ago to match MS.
    svn path=/trunk/gert/; revision=101031

--- standalone/bug81453/test.cs
+++ standalone/bug81453/test.cs
@@ -10,11 +10,7 @@ class Program
 		Assembly a = Assembly.LoadFile (file);
 		if (a == null)
 			return 1;
-#if MONO
-		if (a.FullName != "libb, Version=0.0.0.0, Culture=neutral")
-#else
 		if (a.FullName != "libb, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null")
-#endif
 			return 2;
 		return 0;
 	}
