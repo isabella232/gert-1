e0890dbe30e374810c31973f72cf92a0fe597ab5
Gert Driesen <drieseng@users.sourceforge.net>
2008-04-18 10:28:52 +0000

    Improved test.
    svn path=/trunk/gert/; revision=101109

--- standalone/bug81767/test.cs
+++ standalone/bug81767/test.cs
@@ -7,8 +7,20 @@ class Program
 {
 	static int Main ()
 	{
+		if (!RunTest (DeleteThread, true))
+			return 1;
+		if (!RunTest (MoveThread, false))
+			return 2;
+		if (!File.Exists (path_backup))
+			return 3;
+		return 0;
+	}
+
+
+	static bool RunTest (ThreadStart tstart, bool delete)
+	{
 		locker = 0;
-		Thread t = new Thread (DeleteThread);
+		Thread t = new Thread (tstart);
 		t.Start ();
 
 		using (FileStream fs = File.Open (path, FileMode.Create, FileAccess.Write, FileShare.Delete)) {
@@ -29,28 +41,38 @@ class Program
 				temp.GetString (b);
 				Thread.Sleep (1000);
 			}
-			
-			if (!File.Exists (path))
+
+			if (delete && !File.Exists (path))
 				throw new Exception ("File should continue to exist until FileStream is closed.");
 		}
 		t.Join ();
 
-		if (File.Exists (path))
-			return 1;
-		return 0;
+		return (!File.Exists (path));
 	}
 
 	static void DeleteThread ()
 	{
-		while (locker == 0) {
+		while (locker == 0)
 			Thread.Sleep (100);
-		}
 		File.Delete (path);
 		if (!File.Exists (path))
 			throw new Exception ("File should continue to exist until FileStream is closed.");
 	}
 
+	static void MoveThread ()
+	{
+		while (locker == 0)
+			Thread.Sleep (100);
+		File.Move (path, path_backup);
+		if (File.Exists (path))
+			throw new Exception ("File should immediately be moved.");
+		if (!File.Exists (path_backup))
+			throw new Exception ("File should have immediately been created.");
+	}
+
 	static byte locker;
 	static string path = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
 		"file.txt");
+	static string path_backup = Path.Combine (AppDomain.CurrentDomain.BaseDirectory,
+		"file.backup");
 }
