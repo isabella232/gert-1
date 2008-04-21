c520f31ce3222910aca1c84f928ac80c585d9107
Gert Driesen <drieseng@users.sourceforge.net>
2008-04-21 19:15:50 +0000

    Fixed test.
    svn path=/trunk/gert/; revision=101359

--- standalone/bug378924/test.cs
+++ standalone/bug378924/test.cs
@@ -6,6 +6,6 @@ class Program
 	static void Main (string [] args)
 	{
 		AssemblyName an = AssemblyName.GetAssemblyName (args [0]);
-		Assert.AreEqual ("mscorlib, Version=2.1.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", an.FullName, "#1");
+		Assert.AreEqual ("mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", an.FullName, "#1");
 	}
 }
