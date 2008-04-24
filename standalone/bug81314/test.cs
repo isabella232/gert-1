f0eac20d27b05361d9549efb122ae6383b52d1dd
Gert Driesen <drieseng@users.sourceforge.net>
2008-04-24 18:09:39 +0000

    Improve test.
    svn path=/trunk/gert/; revision=101718

--- standalone/bug81314/test.cs
+++ standalone/bug81314/test.cs
@@ -31,38 +31,30 @@ class TinyHost : MarshalByRefObject
 		StringWriter sw = new StringWriter ();
 		h.Execute ("Default.aspx", sw);
 		string result = sw.ToString ();
-		if (result.IndexOf (">About<") != -1) {
-			Console.WriteLine (result);
-			return 1;
-		}
-		if (result.IndexOf ("title=\"OmschrijvingAbout!\"") != -1) {
-			Console.WriteLine (result);
-			return 2;
-		}
-		if (result.IndexOf (">Tester<") != -1) {
-			Console.WriteLine (result);
-			return 3;
-		}
-		if (result.IndexOf ("title=\"OmschrijvingTester!\"") != -1) {
-			Console.WriteLine (result);
-			return 4;
-		}
-		if (result.IndexOf (">Titel!Admins<") != -1) {
-			Console.WriteLine (result);
-			return 5;
-		}
-		if (result.IndexOf ("title=\"Omschrijving!Admins\"") != -1) {
-			Console.WriteLine (result);
-			return 6;
-		}
-		if (result.IndexOf (">Auth Users Only<") != -1) {
-			Console.WriteLine (result);
-			return 7;
-		}
-		if (result.IndexOf ("Error") != -1) {
-			Console.WriteLine (result);
-			return 8;
-		}
+
+		Assert.IsFalse (result.IndexOf (">About<") != -1, "#A1:" + result);
+		Assert.IsFalse (result.IndexOf ("title=\"OmschrijvingAbout!\"") != -1, "#A2:" + result);
+		Assert.IsFalse (result.IndexOf (">Tester<") != -1, "#A3:" + result);
+		Assert.IsFalse (result.IndexOf ("title=\"OmschrijvingTester!\"") != -1, "#A4:" + result);
+		Assert.IsFalse (result.IndexOf (">Titel!Admins<") != -1, "#A5:" + result);
+		Assert.IsFalse (result.IndexOf ("title=\"Omschrijving!Admins\"") != -1, "#A6:" + result);
+		Assert.IsFalse (result.IndexOf (">Admins<") != -1, "#A7:" + result);
+		Assert.IsFalse (result.IndexOf (">Auth Users Only<") != -1, "#A8:" + result);
+		Assert.IsFalse (result.IndexOf ("Error") != -1, "#A9:" + result);
+
+		sw.GetStringBuilder ().Length = 0;
+		h.Execute ("Index.aspx", sw);
+		result = sw.ToString ();
+
+		Assert.IsTrue (result.IndexOf (">About<") != -1, "#B1:" + result);
+		Assert.IsTrue (result.IndexOf ("title=\"OmschrijvingAbout!\"") != -1, "#B2:" + result);
+		Assert.IsTrue (result.IndexOf (">Tester<") != -1, "#B3:" + result);
+		Assert.IsTrue (result.IndexOf ("title=\"OmschrijvingTester!\"") != -1, "#B4:" + result);
+		Assert.IsFalse (result.IndexOf (">Titel!Admins<") != -1, "#B5:" + result);
+		Assert.IsFalse (result.IndexOf ("title=\"Omschrijving!Admins\"") != -1, "#B6:" + result);
+		Assert.IsTrue (result.IndexOf (">Admins<") != -1, "#B7:" + result);
+		Assert.IsTrue (result.IndexOf (">Auth Users Only<") != -1, "#B8:" + result);
+		Assert.IsFalse (result.IndexOf ("Error") != -1, "#B9:" + result);
 
 		return 0;
 	}
