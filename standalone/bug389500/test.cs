521e935c8ce356e92f73987106499b474c099210
Gert Driesen <drieseng@users.sourceforge.net>
2008-05-12 19:17:11 +0000

    Added test for bug #389500.
    svn path=/trunk/gert/; revision=103025

---
+++ standalone/bug389500/test.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+	static int Main (string [] args)
+	{
+		string basedir = AppDomain.CurrentDomain.BaseDirectory;
+
+		if (args.Length == 0) {
+			Process p = new Process ();
+			p.StartInfo.FileName = Path.Combine (basedir, "test.exe");
+			p.StartInfo.Arguments = "fork";
+			p.Start ();
+			p.WaitForExit ();
+			Assert.AreEqual (666, p.ExitCode, "exit code");
+		} else if (args.Length == 1) {
+			if (args [0] != "fork") {
+				Process p = new Process ();
+				p.StartInfo.FileName = args [0];
+				p.StartInfo.Arguments = Path.Combine (basedir, "test.exe") + " fork";
+				p.Start ();
+				p.WaitForExit ();
+				Assert.AreEqual (666, p.ExitCode, "exit code");
+			} else {
+				return 666;
+			}
+		} else {
+			return 2;
+		}
+
+		return 0;
+	}
+}
