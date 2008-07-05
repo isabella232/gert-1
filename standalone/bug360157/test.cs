676f04d670b3716b6e3e9229bd2a7ae62656cd21
Gert Driesen <drieseng@users.sourceforge.net>
2008-07-05 18:32:44 +0000

    Further improvements.
    svn path=/trunk/gert/; revision=107298

--- standalone/bug360157/test.cs
+++ standalone/bug360157/test.cs
@@ -8,39 +8,83 @@ class Program
 		if (Environment.GetEnvironmentVariable ("MONO_TESTS_SQL") == null)
 			return 0;
 
-		string connectionString = CreateConnectionString ();
+		string server;
+		string connectionString;
+		DateTime start;
+		TimeSpan elapsed;
 
-		DateTime start = DateTime.Now;
+		server = Environment.GetEnvironmentVariable ("MONO_TESTS_SQL_HOST");
+		if (server == null)
+			throw new ArgumentException ("The MONO_TESTS_SQL_HOST environment variable is not set.");
+
+		connectionString = CreateConnectionString (server);
+
+		start = DateTime.Now;
 
 		try {
 			using (SqlConnection sqlConnection = new SqlConnection (connectionString)) {
 				sqlConnection.Open ();
 			}
-			return 1;
+			Assert.Fail ("#A");
 		} catch (SqlException) {
+			// Cannot open database "DoesNotExist" requested by the login. The login failed.
+			// Login failed for user 'sa'
 		}
 
 		try {
 			using (SqlConnection sqlConnection = new SqlConnection (connectionString)) {
 				sqlConnection.Open ();
 			}
-			return 2;
+			Assert.Fail ("#B");
 		} catch (SqlException) {
 		}
 
-		TimeSpan elapsed = DateTime.Now - start;
-		if (elapsed.TotalMilliseconds > 5000)
-			return 3;
+		elapsed = DateTime.Now - start;
+
+		Assert.IsTrue (elapsed.TotalMilliseconds < 5000, "#C:" + elapsed.TotalMilliseconds);
+
+		connectionString = CreateConnectionString ("ZZZ");
+
+		start = DateTime.Now;
+
+		try {
+			using (SqlConnection sqlConnection = new SqlConnection (connectionString)) {
+				sqlConnection.Open ();
+			}
+			return 4;
+		} catch (SqlException) {
+			// An error has occurred while establishing a connection
+			// to the server.  When connecting to SQL Server 2005,
+			// this failure may be caused by the fact that under the
+			// default settings SQL Server does not allow remote
+			// connections. (provider: Named Pipes Provider, error:
+			// 40 - Could not open a connection to SQL Server)
+		}
+
+		try {
+			using (SqlConnection sqlConnection = new SqlConnection (connectionString)) {
+				sqlConnection.Open ();
+			}
+			return 5;
+		} catch (SqlException) {
+			// An error has occurred while establishing a connection
+			// to the server.  When connecting to SQL Server 2005,
+			// this failure may be caused by the fact that under the
+			// default settings SQL Server does not allow remote
+			// connections. (provider: Named Pipes Provider, error:
+			// 40 - Could not open a connection to SQL Server)
+		}
+
+		elapsed = DateTime.Now - start;
+
+		Assert.IsTrue (elapsed.TotalMilliseconds > 30000, "#F1:" + elapsed.TotalMilliseconds);
+		Assert.IsTrue (elapsed.TotalMilliseconds < 80000, "#F2:" + elapsed.TotalMilliseconds);
 
 		return 0;
 	}
 
-	static string CreateConnectionString ()
+	static string CreateConnectionString (string server)
 	{
-		string server = Environment.GetEnvironmentVariable ("MONO_TESTS_SQL_HOST");
-		if (server == null)
-			throw new ArgumentException ("The MONO_TESTS_SQL_HOST environment variable is not set.");
-
 		string user = Environment.GetEnvironmentVariable ("MONO_TESTS_SQL_USER");
 		if (user == null)
 			throw new ArgumentException ("The MONO_TESTS_SQL_USER environment variable is not set.");
@@ -50,7 +94,7 @@ class Program
 			throw new ArgumentException ("The MONO_TESTS_SQL_PWD environment variable is not set.");
 
 		return string.Format ("Server={0};Database=DoesNotExist;Pooling=true;" +
-			"Connection Lifetime=60;Connect Timeout=500;Max Pool Size=1;UID={1};" +
+			"Connection Lifetime=60;Connect Timeout=25;Max Pool Size=1;UID={1};" +
 			"Password={2}", server, user, pwd);
 	}
 }
