8eb21b230fcdffc3e057e760a7f1b09a147f8eae
Gert Driesen <drieseng@users.sourceforge.net>
2008-07-26 16:43:39 +0000

    Improve test.
    svn path=/trunk/gert/; revision=108860

--- standalone/bug396486/test.cs
+++ standalone/bug396486/test.cs
@@ -5,15 +5,20 @@ class Program
 {
 	static void Main ()
 	{
+		const string issuerName = "C=PL, S=Somewhere, O=SomeON, OU=SomeOUN, CN=test, E=test@test";
+#if NET_2_0
 		const string issuer = "E=test@test, CN=test, OU=SomeOUN, O=SomeON, S=Somewhere, C=PL";
+#endif
 
 		X509Certificate cert = X509Certificate.CreateFromCertFile ("cert.pem");
 #if NET_2_0
 		Assert.AreEqual (issuer, cert.Issuer, "#1");
 		Assert.AreEqual (issuer, cert.Subject, "#2");
+		Assert.AreEqual (issuerName, cert.GetIssuerName (), "#3");
+		Assert.AreEqual (issuerName, cert.GetName (), "#4");
 #else
-		Assert.AreEqual (issuer, cert.GetIssuerName (), "#1");
-		Assert.AreEqual (issuer, cert.GetName (), "#2");
+		Assert.AreEqual (issuerName, cert.GetIssuerName (), "#1");
+		Assert.AreEqual (issuerName, cert.GetName (), "#2");
 #endif
 	}
 }
