5049fd66a0ab2ef199b576474b00993d215b669a
Gert Driesen <drieseng@users.sourceforge.net>
2008-09-02 17:30:28 +0000

    Added test for second part of bug.
    svn path=/trunk/gert/; revision=112105

--- standalone/bug81932/test.cs
+++ standalone/bug81932/test.cs
@@ -1,11 +1,18 @@
 using System;
 using System.IO;
 using System.Xml;
+using System.Xml.Schema;
 using System.Xml.XPath;
 
 class Program
 {
-	static int Main ()
+	static void Main ()
+	{
+		Test1 ();
+		Test2 ();
+	}
+
+	static void Test1 ()
 	{
 		string xml = "<root><child1><nest1><nest2>hello!</nest2></nest1></child1><child2/><child3/></root>";
 		XmlReader r = new XmlTextReader (new StringReader (xml));
@@ -20,14 +27,21 @@ class Program
 		string expected = string.Format ("<child1>{0}  <nest1>{0}    <nest2>hello!</nest2>{0}  </nest1>{0}</child1>{0}<child2 />{0}<child3 />",
 			Environment.NewLine);
 
-		if (n.OuterXml != expected) {
-			Console.WriteLine ("Expected result:");
-			Console.WriteLine (expected);
-			Console.WriteLine ();
-			Console.WriteLine ("Actual result:");
-			Console.WriteLine (n.OuterXml);
-			return 1;
-		}
-		return 0;
+		Assert.AreEqual (expected, n.OuterXml, "#1");
+	}
+
+	static void Test2 ()
+	{
+		XmlSchemaSet schemaSet = new XmlSchemaSet ();
+		schemaSet.Add (null, "test.xsd");
+
+		XmlReaderSettings settings = new XmlReaderSettings ();
+		settings.ValidationType = ValidationType.Schema;
+		settings.CloseInput = true;
+		settings.Schemas.Add (schemaSet);
+
+		XmlReader r = XmlReader.Create ("test.xml", settings);
+		XPathDocument d = new XPathDocument (r);
+		d.CreateNavigator ();
 	}
 }
