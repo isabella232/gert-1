82b76c78c368eeb90b37d85485c797fd0a3bf2f9
Gert Driesen <drieseng@users.sourceforge.net>
2008-11-01 19:07:23 +0000

    Improve test.
    svn path=/trunk/gert/; revision=117617

--- standalone/bug440781/test.cs
+++ standalone/bug440781/test.cs
@@ -10,5 +10,23 @@ class Program
 		int len = basedir.Length;
 		Assert.IsTrue (len > 0, "#2");
 		Assert.AreEqual (Path.DirectorySeparatorChar, basedir [len -1], "#3");
+
+		string subdir = Path.Combine (basedir, "sub");
+		File.Copy (Path.Combine (basedir, "remote.dll"),
+			Path.Combine (subdir, "remote.dll"), true);
+
+		AppDomainSetup setup = new AppDomainSetup ();
+		setup.ApplicationBase = subdir;
+
+		AppDomain domain = AppDomain.CreateDomain ("test",
+			AppDomain.CurrentDomain.Evidence, setup);
+		try {
+			RemoteTester remote = (RemoteTester) domain.CreateInstanceAndUnwrap (
+				typeof (RemoteTester).Assembly.FullName,
+				typeof (RemoteTester).FullName);
+			Assert.AreEqual (subdir, remote.GetBaseDirectory (), "#4");
+		} finally {
+			AppDomain.Unload (domain);
+		}
 	}
 }
