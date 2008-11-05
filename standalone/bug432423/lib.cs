165e978ee5980a62999e12ee81fa25c5e117b87b
Gert Driesen <drieseng@users.sourceforge.net>
2008-11-05 21:04:16 +0000

    Allow test to run on .NET 1.x.
    svn path=/trunk/gert/; revision=118037

--- standalone/bug432423/lib.cs
+++ standalone/bug432423/lib.cs
@@ -1,6 +1,12 @@
 using System.Reflection;
 
+#if NET_2_0
+#pragma warning disable 1699
+#endif
 [assembly: AssemblyKeyFile ("test.snk")]
+#if NET_2_0
+#pragma warning restore 1699
+#endif
 
 class Foo
 {
