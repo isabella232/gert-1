c2e0bbc36128fadd557d4256b20a13b3fade9b63
Gert Driesen <drieseng@users.sourceforge.net>
2008-11-05 21:11:35 +0000

    Improve test.
    svn path=/trunk/gert/; revision=118038

--- standalone/bug432423/lib.cs
+++ standalone/bug432423/lib.cs
@@ -1,12 +1,6 @@
 using System.Reflection;
 
-#if NET_2_0
-#pragma warning disable 1699
-#endif
 [assembly: AssemblyKeyFile ("test.snk")]
-#if NET_2_0
-#pragma warning restore 1699
-#endif
 
 class Foo
 {
--- standalone/bug432423/test.cs
+++ standalone/bug432423/test.cs
@@ -10,10 +10,19 @@ class Program
 	{
 		string basedir = AppDomain.CurrentDomain.BaseDirectory;
 
-		AssemblyDefinition assembly = AssemblyFactory.GetAssembly (Path.Combine (basedir, "lib.dll"));
-		Assert.AreEqual (AssemblyFlags.PublicKey, assembly.Name.Flags, "#1");
+		AssemblyDefinition assembly;
+		Assembly a;
 
-		Assembly a = Assembly.LoadFrom (Path.Combine (basedir, "lib.dll"));
-		Assert.AreEqual (AssemblyNameFlags.PublicKey, a.GetName ().Flags, "#2");
+		assembly = AssemblyFactory.GetAssembly (Path.Combine (basedir, "lib.dll"));
+		Assert.AreEqual (AssemblyFlags.PublicKey, assembly.Name.Flags, "#A1");
+
+		a = Assembly.LoadFrom (Path.Combine (basedir, "lib.dll"));
+		Assert.AreEqual (AssemblyNameFlags.PublicKey, a.GetName ().Flags, "#A2");
+
+		assembly = AssemblyFactory.GetAssembly (Path.Combine (basedir, "test.exe"));
+		Assert.AreEqual (AssemblyFlags.SideBySideCompatible, assembly.Name.Flags, "#B1");
+
+		a = Assembly.LoadFrom (Path.Combine (basedir, "test.exe"));
+		Assert.AreEqual (AssemblyNameFlags.PublicKey, a.GetName ().Flags, "#B2");
 	}
 }
