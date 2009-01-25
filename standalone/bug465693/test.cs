585854f71fb8332669717d868c5d132d085a957e
Gert Driesen <drieseng@users.sourceforge.net>
2009-01-25 18:06:47 +0000

    Improve test.
    svn path=/trunk/gert/; revision=124435

--- standalone/bug465693/test.cs
+++ standalone/bug465693/test.cs
@@ -14,6 +14,8 @@ class Program
 
 		HttpWebRequest request;
 
+		File.Copy (Path.Combine (baseDir, "SiteMapFile.sitemap"),
+			Path.Combine (webDir, "Web.sitemap"), true);
 		File.Copy (Path.Combine (baseDir, "nested_V1.sitemap"),
 			Path.Combine (webDir, "nested.sitemap"), true);
 
@@ -48,7 +50,7 @@ class Program
 			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
 			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
 				string result = sr.ReadToEnd ();
-				Assert.AreEqual ("TestV2", result, "#A");
+				Assert.AreEqual ("TestV2", result, "#B");
 			}
 			response.Close ();
 		} catch (WebException ex) {
@@ -59,6 +61,66 @@ class Program
 				}
 			}
 			return 2;
+		}
+
+		File.Copy (Path.Combine (baseDir, "SiteMapFile_Empty.sitemap"),
+			Path.Combine (webDir, "Web.sitemap"), true);
+		Thread.Sleep (1000);
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/index.aspx");
+		request.Method = "GET";
+
+		try {
+			request.GetResponse ();
+			Assert.Fail ("#C1");
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			Assert.IsNotNull (response, "#C2");
+			using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+				string result = sr.ReadToEnd ();
+				Assert.IsTrue (result.IndexOf ("ConfigurationErrorsException") != -1, "#C3:" + result);
+				Assert.IsTrue (result.IndexOf ("The 'siteMapFile' attribute cannot be an empty string.") != -1, "#C4:" + result);
+			}
+		}
+
+		File.Copy (Path.Combine (baseDir, "SiteMapFile_InvalidExtension.sitemap"),
+			Path.Combine (webDir, "Web.sitemap"), true);
+		Thread.Sleep (1000);
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/index.aspx");
+		request.Method = "GET";
+
+		try {
+			request.GetResponse ();
+			Assert.Fail ("#D1");
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			Assert.IsNotNull (response, "#D2");
+			using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+				string result = sr.ReadToEnd ();
+				Assert.IsTrue (result.IndexOf ("InvalidOperationException") != -1, "#D3:" + result);
+				Assert.IsTrue (result.IndexOf ("The file /index.aspx has an invalid extension, only .sitemap files are allowed in XmlSiteMapProvider.") != -1, "#D4:" + result);
+			}
+		}
+
+		File.Copy (Path.Combine (baseDir, "SiteMapFile_NotFound.sitemap"),
+			Path.Combine (webDir, "Web.sitemap"), true);
+		Thread.Sleep (1000);
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/index.aspx");
+		request.Method = "GET";
+
+		try {
+			request.GetResponse ();
+			Assert.Fail ("#E1");
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			Assert.IsNotNull (response, "#E2");
+			using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+				string result = sr.ReadToEnd ();
+				Assert.IsTrue (result.IndexOf ("InvalidOperationException") != -1, "#E3:" + result);
+				Assert.IsTrue (result.IndexOf ("The file /doesnotexist.sitemap required by XmlSiteMapProvider does not exist.") != -1, "#E4:" + result);
+			}
 		}
 
 		return 0;
