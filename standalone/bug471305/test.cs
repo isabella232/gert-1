fe74090abba738c2379395dfff92a87194078f1e
Gert Driesen <drieseng@users.sourceforge.net>
2009-02-01 09:11:57 +0000

    Added test for bug #471305.
    svn path=/trunk/gert/; revision=125248

---
+++ standalone/bug471305/test.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+
+class Program
+{
+	static int Main (string [] args)
+	{
+		HttpWebRequest request;
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Default.aspx");
+		request.Method = "GET";
+
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+				string expect = "<span>label</span>";
+				int index = result.IndexOf (expect);
+				Assert.IsTrue (index != -1, "#A1:" + result);
+				index = result.IndexOf (expect, index + expect.Length);
+				Assert.AreEqual (-1, index, "#A2:" + result);
+			}
+			response.Close ();
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			if (response != null) {
+				using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+					Console.WriteLine (sr.ReadToEnd ());
+				}
+			}
+			return 1;
+		}
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Index.aspx");
+		request.Method = "GET";
+
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+				string expect = "<span>label</span>";
+				Assert.AreEqual (-1, result.IndexOf (expect), "#B:" + result);
+			}
+			response.Close ();
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			if (response != null) {
+				using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+					Console.WriteLine (sr.ReadToEnd ());
+				}
+			}
+			return 1;
+		}
+
+		return 0;
+	}
+}
---
+++ standalone/bug471305/web/Index.aspx.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+public class _Index : Page
+{
+	protected HtmlForm form1;
+
+	public class CustomControl : Control
+	{
+		protected override void OnInit (EventArgs e)
+		{
+			Label label = new Label ();
+			label.Text = "label";
+			Controls.Add (label);
+			base.OnInit (e);
+		}
+	}
+
+	protected void Page_Load (object sender, EventArgs e)
+	{
+		CustomControl ctrl = new CustomControl ();
+		form1.Controls.Add (ctrl);
+		form1.Controls.Remove (ctrl);
+	}
+}
