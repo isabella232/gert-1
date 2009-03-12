13f8a9f595b4aea5e34d6c4d5690a497f4bf0464
Gert Driesen <drieseng@users.sourceforge.net>
2009-03-12 20:18:57 +0000

    Improve test.
    svn path=/trunk/gert/; revision=129197

--- standalone/bug484082/test.cs
+++ standalone/bug484082/test.cs
@@ -1,15 +1,17 @@
 using System;
+using System.Collections;
 using System.IO;
 using System.Threading;
 
 class Program
 {
-	static FileSystemEventArgs created;
+	static ArrayList created = new ArrayList ();
 
 	static void Main ()
 	{
 		string basedir = AppDomain.CurrentDomain.BaseDirectory;
 		string watchdir = Path.Combine (basedir, "watch");
+		string subdir;
 
 		if (!Directory.Exists (watchdir))
 			Directory.CreateDirectory (watchdir);
@@ -25,32 +27,55 @@ class Program
 		Directory.CreateDirectory (Path.Combine (basedir, "temp"));
 
 		Thread.Sleep (1000);
-		Assert.IsNull (created, "#1");
+		Assert.AreEqual (0, created.Count, "#1");
 
 		Directory.CreateDirectory (Path.Combine (watchdir, "a"));
 
 		Thread.Sleep (1000);
-		Assert.IsNotNull (created, "#2");
-		created = null;
+		Assert.AreEqual (1, created.Count, "#2");
+		created.Clear ();
 
 		Directory.CreateDirectory (Path.Combine (watchdir, "b"));
 
 		Thread.Sleep (1000);
-		Assert.IsNull (created, "#3");
+		Assert.AreEqual (0, created.Count, "#3");
 
 		Directory.CreateDirectory (Path.Combine (Path.Combine (watchdir, "c"), "d"));
 
 		Thread.Sleep (1000);
-		Assert.IsNull (created, "#4");
+		Assert.AreEqual (0, created.Count, "#4");
 
 		Directory.CreateDirectory (Path.Combine (Path.Combine (watchdir, "d"), "a"));
 
 		Thread.Sleep (1000);
-		Assert.IsNotNull (created, "#5");
+		Assert.AreEqual (1, created.Count, "#5");
+
+		subdir = Path.Combine (Path.Combine (watchdir, "e"), "a");
+		Directory.CreateDirectory (subdir);
+		File.Create (Path.Combine (subdir, "xa"));
+
+		Thread.Sleep (1000);
+		Assert.AreEqual (1, created.Count, "#6");
+		created.Clear ();
+
+		subdir = Path.Combine (Path.Combine (watchdir, "f"), "a");
+		Directory.CreateDirectory (subdir);
+		File.Create (Path.Combine (subdir, "ax"));
+
+		Thread.Sleep (1000);
+		Assert.AreEqual (2, created.Count, "#7");
+		created.Clear ();
+
+		subdir = Path.Combine (Path.Combine (watchdir, "g"), "b");
+		Directory.CreateDirectory (subdir);
+		File.Create (Path.Combine (subdir, "ax"));
+
+		Thread.Sleep (1000);
+		Assert.AreEqual (1, created.Count, "#8");
 	}
 
 	static void OnCreated (object sender, FileSystemEventArgs e)
 	{
-		created = e;
+		created.Add (e);
 	}
 }
