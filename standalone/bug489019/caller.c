0a17c93a183d419f4e9a12e8195985b3462519f0
Gert Driesen <drieseng@users.sourceforge.net>
2009-04-01 18:15:20 +0000

    cleanup
    svn path=/trunk/gert/; revision=130778

--- standalone/bug489019/caller.c
+++ standalone/bug489019/caller.c
@@ -3,12 +3,9 @@
 
 int main (int argc, char *argv[])
 {
-		printf("1=%s\n", argv[1]);
-		printf("2=%s\n", argv[2]);
+		close (0);
+		close (1);
+		close (2);
 
-        close (0);
-        close (1);
-        close (2);
-
-        return execlp (argv[1], argv[1], argv[2], NULL);
+		return execlp (argv[1], argv[1], argv[2], NULL);
 }
