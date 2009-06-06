833d328b84048e2e7557b9f3efcd4aa1c038a31c
Gert Driesen <drieseng@users.sourceforge.net>
2009-06-06 07:48:39 +0000

    Improve test.
    svn path=/trunk/gert/; revision=135591

--- standalone/bug397187/test.cs
+++ standalone/bug397187/test.cs
@@ -17,12 +17,8 @@ class Program
 			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
 				string result = sr.ReadToEnd ();
 
-#if MONO
-				Assert.IsTrue (result.IndexOf ("<link href=\"/App_Themes/Default/StyleSheet.css\" type=\"text/css\" rel=\"stylesheet\" />") != -1, "#1:" + result);
-#else
-				Assert.IsTrue (result.IndexOf ("<link href=\"App_Themes/Default/StyleSheet.css\" type=\"text/css\" rel=\"stylesheet\" />") != -1, "#1:" + result);
-#endif
-				Assert.IsTrue (result.IndexOf ("bug #397187") != -1, "#2:" + result);
+				Assert.IsTrue (result.IndexOf ("<link href=\"App_Themes/Default/StyleSheet.css\" type=\"text/css\" rel=\"stylesheet\" />") != -1, "#A1:" + result);
+				Assert.IsTrue (result.IndexOf ("bug #397187 - default") != -1, "#A2:" + result);
 			}
 			response.Close ();
 		} catch (WebException ex) {
@@ -33,6 +29,50 @@ class Program
 				}
 			}
 			return 1;
+		}
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/admin/Default.aspx");
+		request.Method = "GET";
+
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+
+				Assert.IsTrue (result.IndexOf ("<link href=\"../App_Themes/Admin/StyleSheet.css\" type=\"text/css\" rel=\"stylesheet\" />") != -1, "#B1:" + result);
+				Assert.IsTrue (result.IndexOf ("bug #397187 - admin") != -1, "#B2:" + result);
+			}
+			response.Close ();
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			if (response != null) {
+				using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+					Console.WriteLine (sr.ReadToEnd ());
+				}
+			}
+			return 2;
+		}
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/public/Default.aspx");
+		request.Method = "GET";
+
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+
+				Assert.IsTrue (result.IndexOf ("<link href=\"../App_Themes/Default/StyleSheet.css\" type=\"text/css\" rel=\"stylesheet\" />") != -1, "#C1:" + result);
+				Assert.IsTrue (result.IndexOf ("bug #397187 - public") != -1, "#C2:" + result);
+			}
+			response.Close ();
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			if (response != null) {
+				using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+					Console.WriteLine (sr.ReadToEnd ());
+				}
+			}
+			return 2;
 		}
 
 		return 0;
