7bbcf7dcf6cb1e7d2277e7ea3ec576ad908d4df0
Gert Driesen <drieseng@users.sourceforge.net>
2009-06-12 19:29:58 +0000

    Added test for bug #511882.
    svn path=/trunk/gert/; revision=136026

---
+++ standalone/bug511882/test.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+	static int Main (string [] args)
+	{
+		HttpWebRequest request;
+
+		request = (HttpWebRequest) WebRequest.Create ("http://localhost:8081/Default.aspx");
+		request.Method = "GET";
+
+		try {
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse ();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream (), Encoding.UTF8, true)) {
+				string result = sr.ReadToEnd ();
+
+				Regex regex = new Regex (@"^.*<title>\s*<span>OK</span>\s*</title>.*$", RegexOptions.Singleline);
+				Assert.IsTrue (regex.IsMatch (result), "#1:" + result);
+
+				//Assert.IsTrue (result.IndexOf (@"<title>\s+OK\s</title>.+") != -1, "#1:" + result);
+			}
+			response.Close ();
+		} catch (WebException ex) {
+			HttpWebResponse response = (HttpWebResponse) ex.Response;
+			if (response != null) {
+				using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+					Console.WriteLine (sr.ReadToEnd ());
+				}
+			}
+			return 1;
+		}
+
+		return 0;
+	}
+}
---
+++ standalone/bug511882/web/Default.aspx
@@ -0,0 +1,8 @@
+<%@ Page Language="C#" %>
+<html>
+<head id="Head" runat="server">
+	<title><asp:Label Text="OK" runat="server" /></title>
+</head>
+<body>
+</body>
+</html>
