191cde1db30c186c9a9db013b7dccfa5ffd4e0da
Gert Driesen <drieseng@users.sourceforge.net>
2009-06-25 08:06:54 +0000

    Support concurrent requests.
    svn path=/trunk/gert/; revision=136804

--- build/common/SocketResponder.cs
+++ build/common/SocketResponder.cs
@@ -9,7 +9,7 @@ public delegate byte [] SocketRequestHandler (Socket socket);
 
 class SocketResponder : IDisposable
 {
-	private TcpListener tcpListener;
+	private Socket listener;
 	private readonly IPEndPoint _localEndPoint;
 	private Thread listenThread;
 	private SocketRequestHandler _requestHandler;
@@ -22,22 +22,15 @@ class SocketResponder : IDisposable
 	{
 		_localEndPoint = localEP;
 		_requestHandler = requestHandler;
+		Start ();
 	}
 
-	public IPEndPoint LocalEndPoint
-	{
+	public IPEndPoint LocalEndPoint {
 		get { return _localEndPoint; }
 	}
 
-	public void Dispose ()
-	{
-		Stop ();
-	}
-
-	public bool IsStopped
-	{
-		get
-		{
+	public bool IsStopped {
+		get {
 			lock (_syncRoot) {
 				return _stopped;
 			}
@@ -48,13 +41,19 @@ class SocketResponder : IDisposable
 	{
 		lock (_syncRoot) {
 			if (!_stopped)
-				return;
+				throw new InvalidOperationException ("This instance is already started.");
 			_stopped = false;
-			tcpListener = new TcpListener (LocalEndPoint);
-			tcpListener.Start ();
+
+			listener = new Socket (0, SocketType.Stream, ProtocolType.Tcp);
+			listener.Bind (_localEndPoint);
+			listener.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, 0);
+			listener.SetSocketOption (SocketOptionLevel.Socket, SocketOptionName.NoDelay, 1);
+			listener.SetSocketOption (SocketOptionLevel.Socket, SocketOptionName.KeepAlive, 0);
+			listener.Blocking = true;
+			listener.Listen (-1);
 			listenThread = new Thread (new ThreadStart (Listen));
 			listenThread.Start ();
-			Thread.Sleep (50);
+			Thread.Sleep (500);
 		}
 	}
 
@@ -64,12 +63,23 @@ class SocketResponder : IDisposable
 			if (_stopped)
 				return;
 			_stopped = true;
-			if (tcpListener != null) {
-				tcpListener.Stop ();
-				tcpListener = null;
+			if (listener != null) {
+				try {
+					if (listener.Connected) {
+						listener.Shutdown (SocketShutdown.Send);
+					}
+					listener.Close ();
+				} catch { }
+				listener = null;
 				Thread.Sleep (100);
 			}
+			listenThread.Join ();
 		}
+	}
+
+	public void Dispose ()
+	{
+		Stop ();
 	}
 
 	private void Listen ()
@@ -78,21 +88,49 @@ class SocketResponder : IDisposable
 			Socket socket = null;
 
 			try {
-				socket = tcpListener.AcceptSocket ();
-				socket.Send (_requestHandler (socket));
-				try {
-					socket.Shutdown (SocketShutdown.Receive);
-					socket.Shutdown (SocketShutdown.Send);
-				} catch {
-				}
+				socket = listener.Accept ();
+			} catch (SocketException ex) {
+				// ignore interruption of blocking call
+				if (ex.ErrorCode != SOCKET_CLOSED)
+					throw;
+				break;
+			}
+
+			RequestProcessor processor = new RequestProcessor (socket,
+				_requestHandler);
+			Thread t = new Thread (new ThreadStart (processor.Process));
+			t.Start ();
+		}
+	}
+
+	class RequestProcessor
+	{
+		private Socket _socket;
+		private SocketRequestHandler _handler;
+
+		public RequestProcessor (Socket socket, SocketRequestHandler handler)
+		{
+			_socket = socket;
+			_handler = handler;
+		}
+
+		public void Process ()
+		{
+			try {
+				_socket.Send (_handler (_socket));
 			} catch (SocketException ex) {
 				// ignore interruption of blocking call
 				if (ex.ErrorCode != SOCKET_CLOSED)
 					throw;
 			} finally {
+				try {
+					_socket.Shutdown (SocketShutdown.Receive);
+					_socket.Shutdown (SocketShutdown.Send);
+				} catch {
+				}
+
 				Thread.Sleep (100);
-				if (socket != null)
-					socket.Close ();
+				_socket.Close ();
 				Thread.Sleep (100);
 			}
 		}
